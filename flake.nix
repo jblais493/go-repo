43686f8067adad37bfa3e92cb2a7e3b3c7febe84
Joshua Blais <josh@joshblais.com>
2025-09-29 12:14:30 -0600

    flake built

--- flake.nix
+++ flake.nix
@@ -1,15 +1,49 @@
 {
-  description = "A very basic flake";
+  description = "A quick scaffolding system for go projects";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages.${system}.default = pkgs.buildGoModule {
+        pname = "scaffold";
+        version = "1.0.0";
+        src = ./.;
 
-  outputs = { self, nixpkgs }: {
+        vendorHash = "sha256-kuheizBlwHUpozXPmFnoYOJ4zzyUDu3U5k8j0N1bhGc=";
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+        # Critical: Wrap binary with runtime dependencies
+        nativeBuildInputs = [ pkgs.makeWrapper ];
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+        postInstall = ''
+          # Rename binary
+          mv $out/bin/go-repo $out/bin/scaffold
 
-  };
+          # Wrap with required tools in PATH
+          wrapProgram $out/bin/scaffold \
+            --prefix PATH : ${pkgs.lib.makeBinPath [
+              pkgs.git
+              pkgs.gh
+              pkgs.devenv
+              pkgs.nix
+              pkgs.direnv
+              pkgs.age
+            ]}
+        '';
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          go
+          gopls
+          git
+          gh
+          devenv
+          age
+          direnv
+        ];
+      };
+    };
 }
